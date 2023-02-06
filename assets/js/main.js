e33d3985d813c9604b52263717020ddc9ed76645
Miroslav022 <95857090+Miroslav022@users.noreply.github.com>
2023-02-06 21:23:29 +0100

    new

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,19 @@
+//Hamburger menu
+let hamburger = document.querySelector(".menu-btn");
+let sideMenu = document.querySelector(".side-menu");
+hamburger.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (hamburger.classList.contains("open")) {
+    hamburger.classList.remove("open");
+    sideMenu.classList.add("none");
+  } else {
+    hamburger.classList.add("open");
+    sideMenu.classList.remove("none");
+  }
+});
+
+// Best buy section slider
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   direction: "horizontal",
