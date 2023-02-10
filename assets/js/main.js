b71d9c1d8e67ebc9b7de1da99ef96342cabd81d3
Miroslav022 <95857090+Miroslav022@users.noreply.github.com>
2023-02-10 09:05:43 +0100

    shop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,43 @@
 //Elementi
-let navLarge = document.querySelector('#navLarge');
-let navSmall = document.querySelector('#navSmall');
-let header = document.querySelector('header');
+let navLarge = document.querySelector("#navLarge");
+let navSmall = document.querySelector("#navSmall");
+let header = document.querySelector("header");
 
-let headerSection = document.querySelector('.header');
-let recension = document.querySelector('.recension');
-let rcBlock = document.querySelectorAll('.rc-block');
+let headerSection = document.querySelector(".header");
+let recension = document.querySelector(".recension");
+let rcBlock = document.querySelectorAll(".rc-block");
+let categorySec = document.querySelector(".category-ct");
 
+let moveRight = document.querySelector(".block-right");
+let moveLeft = document.querySelector(".block-left");
 
-let moveRight = document.querySelector('.block-right'); 
-let moveLeft = document.querySelector('.block-left');
-
-
-window.addEventListener('load', function(){
+window.addEventListener("load", function () {
   //Navigacija
-  let navAjax = fetch("assets/json/navigation.json").then(response => response.json()).then(data=>{
-    ispisNavigacije(data)
-  })
+  let navAjax = fetch("assets/json/navigation.json")
+    .then((response) => response.json())
+    .then((data) => {
+      ispisNavigacije(data);
+    });
 
   //Sticky navigation (Intersection API)
-  function stickyNav(entries){
+  function stickyNav(entries) {
     const [entry] = entries;
-    if(!entry.isIntersecting) {
-      header.classList.add('sticky')
-    } else header.classList.remove('sticky')
+    if (!entry.isIntersecting) {
+      header.classList.add("sticky");
+    } else header.classList.remove("sticky");
   }
-  let headerObserver = new IntersectionObserver(stickyNav, {root: null, threshold:0});
-  headerObserver.observe(headerSection)
+  let headerObserver = new IntersectionObserver(stickyNav, {
+    root: null,
+    threshold: 0,
+  });
+  headerObserver.observe(headerSection);
 
   //Hamburger menu
   let hamburger = document.querySelector(".menu-btn");
   let sideMenu = document.querySelector(".side-menu");
   hamburger.addEventListener("click", function (e) {
     e.preventDefault();
-  
+
     if (hamburger.classList.contains("open")) {
       hamburger.classList.remove("open");
       sideMenu.classList.add("none");
@@ -42,67 +46,96 @@ window.addEventListener('load', function(){
       sideMenu.classList.remove("none");
     }
   });
-  
+
   // Best buy section slider
   const swiper = new Swiper(".swiper", {
     // Optional parameters
     direction: "horizontal",
     loop: true,
-  
+
     // If we need pagination
     pagination: {
       el: ".swiper-pagination",
     },
-  
+
     // Navigation arrows
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-  
+
     // And if we need scrollbar
     scrollbar: {
       el: ".swiper-scrollbar",
     },
   });
 
-  let block=0;
-  let slider = 0
-  let size = rcBlock[0].getBoundingClientRect().width
+  let block = 0;
+  let slider = 0;
+  let size = rcBlock[0].getBoundingClientRect().width;
   //Recension movement
-  moveRight.addEventListener('click', function(e){
-    e.preventDefault()
+  moveRight.addEventListener("click", function (e) {
+    e.preventDefault();
     console.log(size);
-    if(slider<rcBlock.length-1)  {
-    block+=size+16
-    sliderMovement(block)
-    slider++
+    if (slider < rcBlock.length - 1) {
+      block += size + 16;
+      sliderMovement(block);
+      slider++;
     }
-  })
+  });
 
-  moveLeft.addEventListener('click', function(e){
-    e.preventDefault()
-    if(slider!=0)  {
-    block-=size+16
-    sliderMovement(block)
-    slider--
-    } else slider=0;
-  })
+  moveLeft.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (slider != 0) {
+      block -= size + 16;
+      sliderMovement(block);
+      slider--;
+    } else slider = 0;
+  });
 
   function sliderMovement(block) {
-    recension.style.transform=`translateX(-${block}px)`
+    recension.style.transform = `translateX(-${block}px)`;
   }
-  
-  
+
+  //Ispisivanje Kategorija HomePage
+  let categoryAjax = fetch("assets/json/homePageCaregory.json")
+    .then((response) => response.json())
+    .then((data) => ispisKategorija(data));
 
   //Funkcije
   function ispisNavigacije(navigacija) {
-    let SiteLocation = window.location.pathname
-    console.log(SiteLocation);
-    for(let x of navigacija) {
-      navLarge.insertAdjacentHTML('afterbegin', `<li><a href="${x.href}" class="${x.href==SiteLocation ? "active" : ""}">${x.name}</a></li>`);
-      navSmall.insertAdjacentHTML('afterbegin', `<li><a href="${x.href}" class="${x.href==SiteLocation ? "active" : ""}">${x.name}</a></li>`);
+    let SiteLocation = window.location.pathname;
+    SiteLocation = SiteLocation.substring(1);
+    for (let x of navigacija) {
+      navLarge.insertAdjacentHTML(
+        "afterbegin",
+        `<li><a href="${x.href}" class="${
+          x.href == SiteLocation ? "active" : ""
+        }">${x.name}</a></li>`
+      );
+      navSmall.insertAdjacentHTML(
+        "afterbegin",
+        `<li><a href="${x.href}" class="${
+          x.href == SiteLocation ? "active" : ""
+        }">${x.name}</a></li>`
+      );
     }
   }
-})
 
+  function ispisKategorija(kategorije) {
+    for (let kategorija of kategorije) {
+      categorySec.innerHTML += `<div class="md middle-upper ${
+        kategorija.wmark == "Mousepad" ? "diff-right" : ""
+      } ${kategorija.wmark == "Keyboard" ? "diff-left" : ""}">
+      <div class="lt-middle">
+        <img src="${kategorija.img}" alt="" />
+        <p class="wMark">${kategorija.wmark}</p>
+      </div>
+      <div class="rt-middle">
+        <h3 class="cat-h3">${kategorija.naziv}</h3>
+        <a href="" class="btn-i btn-cat">Shop Now</a>
+      </div>
+    </div>`;
+    }
+  }
+});
