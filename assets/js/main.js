145b4eba5d562c905f339be2c94df5532169b132
Miroslav022 <95857090+Miroslav022@users.noreply.github.com>
2023-03-06 22:02:24 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -160,7 +160,7 @@ function ispisNavigacije(navigacija) {
         window.location.pathname.includes("index.html")
           ? "#goToHeader"
           : x.href
-      }" class="nav-hover ${x.href == SiteLocation ? "active" : ""}">${
+      }" class="nav-hover ${x.href == "index.html" ? "active" : ""}">${
         x.name
       }</a><div class="unactive"></div></li>`
     );
